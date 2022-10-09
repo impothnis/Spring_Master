99e776513d3323680574e5f04be2a226e935b1e4
Pavan Pothnis <pavanpothnis@outlook.com>
2022-10-09 18:04:12 +0530

    Rest Controller

---
+++ recommender-api/src/test/java/io/pavan/springbootdemo/recommenderapi/RecommenderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pavan.springbootdemo.recommenderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommenderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
